c4314eb71dc7dd921c6f246c04678d6be4bf2dcb
SoulPlayer23 <venkiteshsanand1920@gmail.com>
2021-07-21 09:45:27 +0530

    Cart functionality remodeled

--- lib/screens/home_detail_page.dart
+++ lib/screens/home_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:catalog_app/models/catalog.dart';
+import 'package:catalog_app/widgets/home_widgets/add_to_cart.dart';
 import 'package:catalog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -21,14 +22,7 @@ class HomeDetailPage extends StatelessWidget {
           buttonPadding: Vx.mH8,
           children: [
             "\$${catalog.price}".text.bold.xl4.red800.make(),
-            ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                            context.theme.buttonColor),
-                        shape: MaterialStateProperty.all(StadiumBorder())),
-                    child: "Add to cart".text.xl.make())
-                .wh(140, 50),
+            AddToCart(catalog: catalog).wh(140, 50),
           ],
         ).p24(),
       ),
