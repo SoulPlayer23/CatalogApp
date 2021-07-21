782495cf6f019770b8b76e6b733d9b71f9d92922
SoulPlayer23 <venkiteshsanand1920@gmail.com>
2021-07-21 10:12:20 +0530

    VxStore used

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:catalog_app/core/store.dart';
 import 'package:catalog_app/screens/cart_page.dart';
 import 'package:catalog_app/screens/home.dart';
 import 'package:catalog_app/screens/login_page.dart';
 import 'package:catalog_app/utils/routes.dart';
 import 'package:catalog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(VxState(store: MyStore(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
